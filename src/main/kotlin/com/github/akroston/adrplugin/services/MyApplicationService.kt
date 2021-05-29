5973cf44f3640223df37ffc952716881618f0269
GitHub Action <action@github.com>
2021-05-29 21:59:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/akroston/adrplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/akroston/adrplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.akroston.adrplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akroston.adrplugin.MyBundle
 
 class MyApplicationService {
 
